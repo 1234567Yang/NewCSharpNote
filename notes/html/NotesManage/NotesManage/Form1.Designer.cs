c932bc1bf6d25fd47548a7957f81ced6556cf025
Yang <113082249+1234567Yang@users.noreply.github.com>
2024-03-27 16:31:38 -0700

    把源码解压放在目录

---
+++ notes/html/NotesManage/NotesManage/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace NotesManage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textEditorControl1 = new ICSharpCode.TextEditor.TextEditorControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_html = new System.Windows.Forms.TextBox();
+            this.textBox_title = new System.Windows.Forms.TextBox();
+            this.textBox_keywords = new System.Windows.Forms.TextBox();
+            this.textBox_descrip = new System.Windows.Forms.TextBox();
+            this.button_add = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_model = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textEditorControl1
+            // 
+            this.textEditorControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textEditorControl1.IsReadOnly = false;
+            this.textEditorControl1.Location = new System.Drawing.Point(12, 230);
+            this.textEditorControl1.Name = "textEditorControl1";
+            this.textEditorControl1.Size = new System.Drawing.Size(598, 257);
+            this.textEditorControl1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 91);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "HTML:\r\n\r\nTITLE:\r\n\r\nDESCRIPTION:\r\n\r\nKEY WORDS(separate by ,):";
+            // 
+            // textBox_html
+            // 
+            this.textBox_html.Location = new System.Drawing.Point(59, 69);
+            this.textBox_html.Name = "textBox_html";
+            this.textBox_html.Size = new System.Drawing.Size(475, 20);
+            this.textBox_html.TabIndex = 2;
+            // 
+            // textBox_title
+            // 
+            this.textBox_title.Location = new System.Drawing.Point(59, 95);
+            this.textBox_title.Name = "textBox_title";
+            this.textBox_title.Size = new System.Drawing.Size(475, 20);
+            this.textBox_title.TabIndex = 5;
+            // 
+            // textBox_keywords
+            // 
+            this.textBox_keywords.Location = new System.Drawing.Point(168, 147);
+            this.textBox_keywords.Name = "textBox_keywords";
+            this.textBox_keywords.Size = new System.Drawing.Size(434, 20);
+            this.textBox_keywords.TabIndex = 6;
+            // 
+            // textBox_descrip
+            // 
+            this.textBox_descrip.Location = new System.Drawing.Point(100, 121);
+            this.textBox_descrip.Name = "textBox_descrip";
+            this.textBox_descrip.Size = new System.Drawing.Size(434, 20);
+            this.textBox_descrip.TabIndex = 7;
+            // 
+            // button_add
+            // 
+            this.button_add.Location = new System.Drawing.Point(250, 187);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(75, 23);
+            this.button_add.TabIndex = 8;
+            this.button_add.Text = "Add";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(232, 503);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Confirm / Write";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "static position:";
+            // 
+            // textBox_model
+            // 
+            this.textBox_model.Location = new System.Drawing.Point(96, 10);
+            this.textBox_model.Name = "textBox_model";
+            this.textBox_model.Size = new System.Drawing.Size(492, 20);
+            this.textBox_model.TabIndex = 11;
+            this.textBox_model.Text = "model.html";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 535);
+            this.Controls.Add(this.textBox_model);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.textBox_descrip);
+            this.Controls.Add(this.textBox_keywords);
+            this.Controls.Add(this.textBox_title);
+            this.Controls.Add(this.textBox_html);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textEditorControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ICSharpCode.TextEditor.TextEditorControl textEditorControl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_html;
+        private System.Windows.Forms.TextBox textBox_title;
+        private System.Windows.Forms.TextBox textBox_keywords;
+        private System.Windows.Forms.TextBox textBox_descrip;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_model;
+    }
+}
+
---
+++ notes/html/NotesManage/NotesManage/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ICSharpCode;
+using ICSharpCode.TextEditor;
+using ICSharpCode.TextEditor.Actions;
+using ICSharpCode.TextEditor.Document;
+
+namespace NotesManage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textEditorControl1.ShowEOLMarkers = false;
+            textEditorControl1.ShowHRuler = false;
+            textEditorControl1.ShowInvalidLines = false;
+            textEditorControl1.ShowMatchingBracket = true;
+            textEditorControl1.ShowSpaces = false;
+            textEditorControl1.ShowTabs = false;
+            textEditorControl1.ShowVRuler = false;
+            textEditorControl1.AllowCaretBeyondEOL = false;
+            textEditorControl1.Document.HighlightingStrategy = HighlightingStrategyFactory.CreateHighlightingStrategy("HTML");
+            textEditorControl1.Encoding = Encoding.UTF8;
+            textEditorControl1.ActiveTextAreaControl.TextArea.KeyUp += new System.Windows.Forms.KeyEventHandler(textEditorControl1_KeyUp);
+        }
+        private void textEditorControl1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.ShiftKey || e.KeyCode == Keys.ControlKey || e.KeyCode == Keys.Alt)
+            {
+                return;
+            }
+            //textEditorControl1.ActiveTextAreaControl.SelectionManager.SelectedText;
+            int pos = textEditorControl1.Document.PositionToOffset(textEditorControl1.ActiveTextAreaControl.Caret.Position);
+            int lineIndex = textEditorControl1.ActiveTextAreaControl.Caret.Position.Line;
+            // 获取当前行的 LineSegment
+            var lineSegment = textEditorControl1.Document.GetLineSegment(lineIndex);
+            // 使用 Document.GetText 方法获取行的文本
+            string lineText = textEditorControl1.Document.GetText(lineSegment.Offset, lineSegment.Length);
+            int cursorPosInLine = textEditorControl1.ActiveTextAreaControl.Caret.Offset - lineSegment.Offset;
+
+            //Console.WriteLine(pos);
+            if (cursorPosInLine > 0 && lineText.Substring(cursorPosInLine - 1, 1) == ">")
+            {
+                // 获取当前行的索引
+                
+                if(lineText.Substring(0, cursorPosInLine).IndexOf("<") != -1)
+                {
+                    if(!IsInQuote(lineText, cursorPosInLine))
+                    {
+                        //Console.WriteLine("!!!");
+                        int p1 = lineText.Substring(0, cursorPosInLine).LastIndexOf("<");
+                        string insert = lineText.Substring(p1 + 1, cursorPosInLine - p1 - 2);
+                        //Console.WriteLine(insert);
+                        if(insert.IndexOf("/") == -1)
+                        {
+                            insert = "</" + insert.Substring(0, (insert.IndexOf(" ") != -1)? insert.IndexOf(" "):insert.Length) + ">";
+                            textEditorControl1.Document.Insert(textEditorControl1.ActiveTextAreaControl.Caret.Offset, insert);
+                        }
+                    }
+                }
+            }
+        }
+        private bool IsInQuote(string text, int pos)
+        {
+            string before = text.Substring(0, pos);
+            return before.Count(c => c == '\"') % 2 != 0;
+        }
+
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            string pos = "raw\\" + this.textBox_html.Text;
+            if (!File.Exists(textBox_model.Text) || !File.Exists(pos))
+            {
+                MessageBox.Show("Position is not correct!");
+                return;
+            }
+            string model = File.ReadAllText(textBox_model.Text);
+            string content = File.ReadAllText(pos);
+            string whole = model;
+            whole = whole.Replace("【TITLE】", this.textBox_title.Text);
+            whole = whole.Replace("【KEYWORDS】", this.textBox_keywords.Text);
+            whole = whole.Replace("【DESCRIPTION】", this.textBox_descrip.Text);
+
+            whole = whole.Replace("【CONTENT】", content);
+
+            this.textEditorControl1.Text = whole;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dir = this.textBox_html.Text.Substring(0, this.textBox_html.Text.IndexOf(".")) + "\\";
+            Directory.CreateDirectory(this.textBox_html.Text.Substring(0, this.textBox_html.Text.IndexOf(".")));
+            File.WriteAllText(dir + "F_" + this.textBox_html.Text,this.textEditorControl1.Text);
+            File.AppendAllText("index.txt", dir + "F_" + this.textBox_html.Text + "|" + 
+                this.textBox_title.Text + "|" + 
+                DateTime.UtcNow.ToString() + 
+                Environment.NewLine);
+
+            if(Directory.Exists("raw\\" + this.textBox_html.Text.Substring(0, this.textBox_html.Text.IndexOf("."))))
+            {
+                if(MessageBox.Show("Also copy the image?", "Question: ", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    foreach(var a in Directory.EnumerateFiles("raw\\" + this.textBox_html.Text.Substring(0, this.textBox_html.Text.IndexOf("."))))
+                    {
+                        File.WriteAllBytes(dir + Path.GetFileName(a), File.ReadAllBytes(a));
+                    }
+                }
+            }
+
+            this.textBox_html.Text = "";
+            this.textBox_title.Text = "";
+            this.textBox_descrip.Text = "";
+            // this.textBox_keywords.Text = "";
+            this.textEditorControl1.Text = "";
+            
+        }
+    }
+}
